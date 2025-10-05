ffd21551220ee7cbb213b5bec2afe1da45028980
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 04:31:59 +0530

    feat: add new spider to extract text content from web pages

---
+++ src/webscraper/spiders/text_extract.py
@@ -0,0 +1,24 @@
+import scrapy
+from urllib.parse import urlparse
+
+class TextExtractSpider(scrapy.Spider):
+    name = "text_extract"
+
+    start_urls = []
+
+    def __init__(self, start_url=None, *args, **kwargs):
+        super(TextExtractSpider, self).__init__(*args, **kwargs)
+
+        if start_url:
+            self.start_urls = [start_url]
+        else:
+            raise ValueError("A start_url must be provided.")
+
+    def parse(self, response):
+        texts = response.css("body *::text").getall()
+        content = " ".join(t.strip() for t in texts if t.strip())
+
+        yield {
+            "url": response.url,
+            "content": content
+        }
