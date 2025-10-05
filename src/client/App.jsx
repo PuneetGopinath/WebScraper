f10c53e5636eadb5d04c4dc6b24a3eb1ab19c31b
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 03:40:27 +0530

    fix: add links to Game and Screenshot components in App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,15 @@
+/**
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/client/App.jsx
+ * License: MIT (see LICENSE)
+ */
+
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import Game from "./components/Game";
+import Screenshot from "./components/Screenshot";
 
 export default function App() {
     return (
@@ -9,6 +17,8 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    <Route path="game" element={<Game />} />
+                    <Route path="ss" element={<Screenshot />} />
                 </Route>
             </Routes>
         </BrowserRouter>
