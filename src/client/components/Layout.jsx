73d13940b54e918a2141944ae5a7b1b995174d9b
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 03:37:08 +0530

    feat: add header and footer to Layout component

--- src/client/components/Layout.jsx
+++ src/client/components/Layout.jsx
@@ -1,11 +1,24 @@
+/**
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/client/components/Layout.jsx
+ * License: MIT (see LICENSE)
+ */
+
 import { Outlet } from "react-router";
 
 export default function Layout() {
     return (
         <>
+            <header>
+                <h1>Web Scraper Suite</h1>
+            </header>
             <main>
                 <Outlet />
             </main>
+            <footer>
+                <p>Puneet Gopinath &copy; 2025</p>
+                <p>Credits to font-awesome as we have used their icons to display the symbol for coins.</p>
+            </footer>
         </>
     );
 };
