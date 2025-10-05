8362c2f7dbca08967f13e3a553070bc858ae6744
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 03:36:27 +0530

    fix: remove heading at top, add another link in Home component

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,9 +1,14 @@
+/**
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/client/components/Home.jsx
+ * License: MIT (see LICENSE)
+ */
+
 import { Link } from "react-router";
 
 export default function Home() {
     return (
         <>
-            <h1>Web Scraper Suite</h1>
             <h5>What is the use of Web Scraper?</h5>
             <p>
                 Web scraping is a technique used to extract large amounts of data from websites where the data is unstructured (HTML format) and convert it into a structured format (Excel, CSV, JSON, etc.) for easier analysis and processing. It is commonly used for various purposes such as market research, price comparison, sentiment analysis, lead generation, and more.
@@ -14,7 +19,8 @@ export default function Home() {
             </p>
             <h5>Using the MVP that I've built, we can only take screenshot of pages and extract text content.</h5>
             <p>But to use this, I've developed a clicker game that allows users to get coins and then spend them on using the features of the web scraper.<br />This step is taken to avoid rate limiting issues if users try to mass scrape pages.</p>
-            <Link to="/game" className="btn">Get coins!</Link>
+            <Link to="/game" className="btn">Start earning coins!</Link>
+            <Link to="/ss" className="btn">Spend your coins!</Link>
         </>
     );
 };
