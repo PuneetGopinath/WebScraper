bb3eb11e6db7bfe13c2031dfcbc66fbb4ca6f807
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 16:02:39 +0530

    docs: add copyright attribution

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,3 +1,9 @@
+/**
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/client/index.jsx
+ * License: MIT (see LICENSE)
+ */
+
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
