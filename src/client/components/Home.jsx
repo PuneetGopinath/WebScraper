ac24041596c6bbd483ef48b224069c73650bcb3c
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 16:39:54 +0530

    feat: improve content in Home component for clarity

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -11,16 +11,26 @@ export default function Home() {
         <>
             <h5>What is the use of Web Scraper?</h5>
             <p>
-                Web scraping is a technique used to extract large amounts of data from websites where the data is unstructured (HTML format) and convert it into a structured format (Excel, CSV, JSON, etc.) for easier analysis and processing. It is commonly used for various purposes such as market research, price comparison, sentiment analysis, lead generation, and more.
+                Web scraping extracts large amounts of unstructured website data (HTML) from websites and converts it into a structured format (Excel, CSV, JSON, etc.) for easier analysis and processing. It is widely used for various purposes such as market research, price comparison, sentiment analysis, lead generation, and more.
             </p>
+            <br />
             <h5>Why use Web Scraper Suite?</h5>
             <p>
-                Web Scraper Suite is a comprehensive tool designed to simplify the web scraping process. It offers an intuitive interface that allows users to easily create and manage scraping tasks without requiring extensive programming knowledge. The suite includes features such as scheduling, data export options, and support for various data formats, making it a versatile solution for both beginners and experienced users.
+                Web Scraper Suite simplifies scraping with intuitive interface and essential features, designed for all kinds of users.
             </p>
-            <h5>Using the MVP that I've built, we can only take screenshot of pages and extract text content.</h5>
+            <br />
+            <h5>Current MVP Features</h5>
+            <p>
+                This MVP enables screenshot capture and text extraction from web pages. To avoid rate limiting, a game lets users earn coins which they spend on scraping features.
+            </p>
+            <br />
+            <h2>PLEASE DON'T PLAY TOO MUCH WITH THE TOOLS, I HAVE JUST 5$ FREE CREDITS IN ZYTE API</h2>
+
             <p>But to use this, I've developed a clicker game that allows users to get coins and then spend them on using the features of the web scraper.<br />This step is taken to avoid rate limiting issues if users try to mass scrape pages.</p>
+            <br />
             <Link to="/game" className="btn">Start earning coins!</Link>
-            <Link to="/ss" className="btn">Spend your coins!</Link>
+            <Link to="/ss" className="btn">Get screenshots!</Link>
+            <Link to="/text" className="btn">Extract text!</Link>
         </>
     );
 };
