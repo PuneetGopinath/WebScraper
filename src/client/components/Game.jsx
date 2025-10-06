8504bfc1d7662db427daa33d6a8826a3f86173fe
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-10-06 16:12:28 +0530

    feat: update price in the Game component, add price & link for extract text

--- src/client/components/Game.jsx
+++ src/client/components/Game.jsx
@@ -22,10 +22,13 @@ export default function Game() {
             <h5>Click the button to earn coins!</h5>
             <button onClick={handleClick}>Click me!</button>
             <p>You can use the coins you earn to access web scraping features.</p>
-            <p>Current Coins: {coins} <BsCoin /></p>
+            <br />
+            <h5>Current Coins: {coins} <BsCoin /></h5>
             <p>(Note: This is a simple MVP. Your coins will reset if you clear browser data.)</p>
-            <h5>50 coins per screenshot you want to get.</h5>
+            <h5>200 coins per screenshot.</h5>
+            <h5>100 coins per text extraction.</h5>
             <Link to="/ss" className="btn">Get Screenshots!</Link>
+            <Link to="/text" className="btn">Extract Text!</Link>
         </>
     );
 };
